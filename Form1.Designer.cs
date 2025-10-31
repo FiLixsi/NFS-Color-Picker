f1ab0cd8f71362c485102a95b80db580ac9ed46d
dsad_ <dsad_@DESKTOP-DSQB2LD>
2025-10-31 16:21:42 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace NFS_Color_Picker
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            trackBarR = new System.Windows.Forms.TrackBar();
+            trackBarG = new System.Windows.Forms.TrackBar();
+            trackBarB = new System.Windows.Forms.TrackBar();
+            trackBarA = new System.Windows.Forms.TrackBar();
+            numR = new System.Windows.Forms.NumericUpDown();
+            numG = new System.Windows.Forms.NumericUpDown();
+            numB = new System.Windows.Forms.NumericUpDown();
+            numA = new System.Windows.Forms.NumericUpDown();
+            lblR = new System.Windows.Forms.Label();
+            lblG = new System.Windows.Forms.Label();
+            lblB = new System.Windows.Forms.Label();
+            lblA = new System.Windows.Forms.Label();
+            panelColor = new NFS_Color_Picker.DoubleBufferedPanel();
+            txtHEXFormat = new System.Windows.Forms.TextBox();
+            txtDecimalFormat = new System.Windows.Forms.TextBox();
+            labelHEX = new System.Windows.Forms.Label();
+            labelDecimal = new System.Windows.Forms.Label();
+            btnCopyHEX = new System.Windows.Forms.Button();
+            btnCopyDecimal = new System.Windows.Forms.Button();
+
+            ((System.ComponentModel.ISupportInitialize)trackBarR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarG).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarA).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numG).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numA).BeginInit();
+            SuspendLayout();
+
+            // trackBarR
+            trackBarR.Location = new System.Drawing.Point(55, 20);
+            trackBarR.Maximum = 255;
+            trackBarR.Name = "trackBarR";
+            trackBarR.Size = new System.Drawing.Size(268, 45);
+            trackBarR.TabIndex = 0;
+            trackBarR.ValueChanged += trackBar_ValueChanged;
+
+            // trackBarG
+            trackBarG.Location = new System.Drawing.Point(55, 60);
+            trackBarG.Maximum = 255;
+            trackBarG.Name = "trackBarG";
+            trackBarG.Size = new System.Drawing.Size(268, 45);
+            trackBarG.TabIndex = 1;
+            trackBarG.ValueChanged += trackBar_ValueChanged;
+
+            // trackBarB
+            trackBarB.Location = new System.Drawing.Point(55, 100);
+            trackBarB.Maximum = 255;
+            trackBarB.Name = "trackBarB";
+            trackBarB.Size = new System.Drawing.Size(268, 45);
+            trackBarB.TabIndex = 2;
+            trackBarB.ValueChanged += trackBar_ValueChanged;
+
+            // trackBarA
+            trackBarA.Location = new System.Drawing.Point(55, 140);
+            trackBarA.Maximum = 255;
+            trackBarA.Name = "trackBarA";
+            trackBarA.Size = new System.Drawing.Size(268, 45);
+            trackBarA.TabIndex = 3;
+            trackBarA.ValueChanged += trackBar_ValueChanged;
+
+            // numR
+            numR.Location = new System.Drawing.Point(357, 20);
+            numR.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numR.Name = "numR";
+            numR.Size = new System.Drawing.Size(130, 23);
+            numR.TabIndex = 4;
+            numR.ValueChanged += numeric_ValueChanged;
+
+            // numG
+            numG.Location = new System.Drawing.Point(357, 60);
+            numG.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numG.Name = "numG";
+            numG.Size = new System.Drawing.Size(130, 23);
+            numG.TabIndex = 5;
+            numG.ValueChanged += numeric_ValueChanged;
+
+            // numB
+            numB.Location = new System.Drawing.Point(357, 105);
+            numB.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numB.Name = "numB";
+            numB.Size = new System.Drawing.Size(130, 23);
+            numB.TabIndex = 6;
+            numB.ValueChanged += numeric_ValueChanged;
+
+            // numA
+            numA.Location = new System.Drawing.Point(357, 147);
+            numA.Maximum = new decimal(new int[] { 255, 0, 0, 0 });
+            numA.Name = "numA";
+            numA.Size = new System.Drawing.Size(130, 23);
+            numA.TabIndex = 7;
+            numA.ValueChanged += numeric_ValueChanged;
+
+            // lblR
+            lblR.AutoSize = true;
+            lblR.Location = new System.Drawing.Point(20, 25);
+            lblR.Name = "lblR";
+            lblR.Size = new System.Drawing.Size(14, 15);
+            lblR.TabIndex = 8;
+            lblR.Text = "R";
+
+            // lblG
+            lblG.AutoSize = true;
+            lblG.Location = new System.Drawing.Point(20, 65);
+            lblG.Name = "lblG";
+            lblG.Size = new System.Drawing.Size(15, 15);
+            lblG.TabIndex = 9;
+            lblG.Text = "G";
+
+            // lblB
+            lblB.AutoSize = true;
+            lblB.Location = new System.Drawing.Point(20, 105);
+            lblB.Name = "lblB";
+            lblB.Size = new System.Drawing.Size(14, 15);
+            lblB.TabIndex = 10;
+            lblB.Text = "B";
+
+            // lblA
+            lblA.AutoSize = true;
+            lblA.Location = new System.Drawing.Point(20, 145);
+            lblA.Name = "lblA";
+            lblA.Size = new System.Drawing.Size(15, 15);
+            lblA.TabIndex = 11;
+            lblA.Text = "A";
+
+            // panelColor
+            panelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panelColor.Location = new System.Drawing.Point(511, 20);
+            panelColor.Name = "panelColor";
+            panelColor.Size = new System.Drawing.Size(150, 150);
+            panelColor.TabIndex = 12;
+
+            // txtHEXFormat
+            txtHEXFormat.Location = new System.Drawing.Point(20, 200);
+            txtHEXFormat.Name = "txtHEXFormat";
+            txtHEXFormat.ReadOnly = false;
+            txtHEXFormat.Size = new System.Drawing.Size(303, 23);
+            txtHEXFormat.TabIndex = 13;
+
+            // txtDecimalFormat
+            txtDecimalFormat.Location = new System.Drawing.Point(20, 250);
+            txtDecimalFormat.Name = "txtDecimalFormat";
+            txtDecimalFormat.ReadOnly = false;
+            txtDecimalFormat.Size = new System.Drawing.Size(303, 23);
+            txtDecimalFormat.TabIndex = 14;
+
+            // labelHEX
+            labelHEX.AutoSize = true;
+            labelHEX.Location = new System.Drawing.Point(20, 180);
+            labelHEX.Name = "labelHEX";
+            labelHEX.Size = new System.Drawing.Size(29, 15);
+            labelHEX.TabIndex = 15;
+            labelHEX.Text = "HEX";
+
+            // labelDecimal
+            labelDecimal.AutoSize = true;
+            labelDecimal.Location = new System.Drawing.Point(20, 230);
+            labelDecimal.Name = "labelDecimal";
+            labelDecimal.Size = new System.Drawing.Size(50, 15);
+            labelDecimal.TabIndex = 16;
+            labelDecimal.Text = "Decimal";
+
+            // btnCopyHEX
+            btnCopyHEX.Location = new System.Drawing.Point(357, 200);
+            btnCopyHEX.Name = "btnCopyHEX";
+            btnCopyHEX.Size = new System.Drawing.Size(130, 25);
+            btnCopyHEX.TabIndex = 17;
+            btnCopyHEX.Text = "Copy";
+            btnCopyHEX.UseVisualStyleBackColor = true;
+            btnCopyHEX.Click += btnCopyHEX_Click;
+
+            // btnCopyDecimal
+            btnCopyDecimal.Location = new System.Drawing.Point(357, 250);
+            btnCopyDecimal.Name = "btnCopyDecimal";
+            btnCopyDecimal.Size = new System.Drawing.Size(130, 25);
+            btnCopyDecimal.TabIndex = 18;
+            btnCopyDecimal.Text = "Copy";
+            btnCopyDecimal.UseVisualStyleBackColor = true;
+            btnCopyDecimal.Click += btnCopyDecimal_Click;
+
+            // Form1
+            ClientSize = new System.Drawing.Size(680, 300);
+            Controls.Add(trackBarR);
+            Controls.Add(trackBarG);
+            Controls.Add(trackBarB);
+            Controls.Add(trackBarA);
+            Controls.Add(numR);
+            Controls.Add(numG);
+            Controls.Add(numB);
+            Controls.Add(numA);
+            Controls.Add(lblR);
+            Controls.Add(lblG);
+            Controls.Add(lblB);
+            Controls.Add(lblA);
+            Controls.Add(panelColor);
+            Controls.Add(txtHEXFormat);
+            Controls.Add(txtDecimalFormat);
+            Controls.Add(labelHEX);
+            Controls.Add(labelDecimal);
+            Controls.Add(btnCopyHEX);
+            Controls.Add(btnCopyDecimal);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "NFS Color Picker v1.0 | By FiLixsi";
+
+            ((System.ComponentModel.ISupportInitialize)trackBarR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarG).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarA).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numG).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numA).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBarR;
+        private System.Windows.Forms.TrackBar trackBarG;
+        private System.Windows.Forms.TrackBar trackBarB;
+        private System.Windows.Forms.TrackBar trackBarA;
+        private System.Windows.Forms.NumericUpDown numR;
+        private System.Windows.Forms.NumericUpDown numG;
+        private System.Windows.Forms.NumericUpDown numB;
+        private System.Windows.Forms.NumericUpDown numA;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblG;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblA;
+        private NFS_Color_Picker.DoubleBufferedPanel panelColor;
+        private System.Windows.Forms.TextBox txtHEXFormat;
+        private System.Windows.Forms.TextBox txtDecimalFormat;
+        private System.Windows.Forms.Label labelHEX;
+        private System.Windows.Forms.Label labelDecimal;
+        private System.Windows.Forms.Button btnCopyHEX;
+        private System.Windows.Forms.Button btnCopyDecimal;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace NFS_Color_Picker
+{
+    public partial class Form1 : Form
+    {
+        private bool isUpdating = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+
+            trackBarR.Value = 0;
+            trackBarG.Value = 0;
+            trackBarB.Value = 0;
+            trackBarA.Value = 0;
+
+            numR.Value = 0;
+            numG.Value = 0;
+            numB.Value = 0;
+            numA.Value = 0;
+
+            txtHEXFormat.TextChanged += TxtHEXFormat_TextChanged;
+            txtDecimalFormat.TextChanged += TxtDecimalFormat_TextChanged;
+            panelColor.Click += panelColor_Click;
+
+            UpdateColor();
+        }
+
+        private void trackBar_ValueChanged(object sender, EventArgs e)
+        {
+            if (isUpdating) return;
+
+            isUpdating = true;
+            if (sender == trackBarR) numR.Value = trackBarR.Value;
+            if (sender == trackBarG) numG.Value = trackBarG.Value;
+            if (sender == trackBarB) numB.Value = trackBarB.Value;
+            if (sender == trackBarA) numA.Value = trackBarA.Value;
+            isUpdating = false;
+
+            UpdateColor();
+        }
+
+        private void numeric_ValueChanged(object sender, EventArgs e)
+        {
+            if (isUpdating) return;
+
+            isUpdating = true;
+            if (sender == numR) trackBarR.Value = (int)numR.Value;
+            if (sender == numG) trackBarG.Value = (int)numG.Value;
+            if (sender == numB) trackBarB.Value = (int)numB.Value;
+            if (sender == numA) trackBarA.Value = (int)numA.Value;
+            isUpdating = false;
+
+            UpdateColor();
+        }
+
+        private void UpdateColor()
+        {
+            if (isUpdating) return;
+            isUpdating = true;
+
+            int r = trackBarR.Value;
+            int g = trackBarG.Value;
+            int b = trackBarB.Value;
+            int a = trackBarA.Value;
+
+            panelColor.BackColor = Color.FromArgb(a, r, g, b);
+
+            string hex = $"{r:X2}{g:X2}{b:X2}{a:X2}";
+            txtHEXFormat.Text = hex;
+
+            uint decimalValue = (uint)((r << 24) | (g << 16) | (b << 8) | a);
+            txtDecimalFormat.Text = decimalValue.ToString();
+
+            isUpdating = false;
+        }
+
+        private void TxtHEXFormat_TextChanged(object sender, EventArgs e)
+        {
+            if (isUpdating) return;
+
+            string hex = txtHEXFormat.Text.Trim().Replace("#", "");
+            if (hex.Length != 8) return;
+
+            try
+            {
+                isUpdating = true;
+                int r = int.Parse(hex.Substring(0, 2), NumberStyles.HexNumber);
+                int g = int.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
+                int b = int.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
+                int a = int.Parse(hex.Substring(6, 2), NumberStyles.HexNumber);
+
+                trackBarR.Value = r; numR.Value = r;
+                trackBarG.Value = g; numG.Value = g;
+                trackBarB.Value = b; numB.Value = b;
+                trackBarA.Value = a; numA.Value = a;
+
+                uint decimalValue = (uint)((r << 24) | (g << 16) | (b << 8) | a);
+                txtDecimalFormat.Text = decimalValue.ToString();
+
+                panelColor.BackColor = Color.FromArgb(a, r, g, b);
+            }
+            catch { }
+            finally { isUpdating = false; }
+        }
+
+        private void TxtDecimalFormat_TextChanged(object sender, EventArgs e)
+        {
+            if (isUpdating) return;
+
+            try
+            {
+                if (!uint.TryParse(txtDecimalFormat.Text.Trim(), out uint decValue))
+                    return;
+
+                isUpdating = true;
+
+                int r = (int)((decValue >> 24) & 0xFF);
+                int g = (int)((decValue >> 16) & 0xFF);
+                int b = (int)((decValue >> 8) & 0xFF);
+                int a = (int)(decValue & 0xFF);
+
+                trackBarR.Value = r; numR.Value = r;
+                trackBarG.Value = g; numG.Value = g;
+                trackBarB.Value = b; numB.Value = b;
+                trackBarA.Value = a; numA.Value = a;
+
+                string hex = $"{r:X2}{g:X2}{b:X2}{a:X2}";
+                txtHEXFormat.Text = hex;
+
+                panelColor.BackColor = Color.FromArgb(a, r, g, b);
+            }
+            catch { }
+            finally { isUpdating = false; }
+        }
+
+        private void btnCopyHEX_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtHEXFormat.Text);
+        }
+
+        private void btnCopyDecimal_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtDecimalFormat.Text);
+        }
+
+        private void panelColor_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog dlg = new ColorDialog())
+            {
+                dlg.Color = panelColor.BackColor;
+                dlg.FullOpen = true;
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    isUpdating = true;
+
+                    Color selected = dlg.Color;
+
+                    trackBarR.Value = selected.R;
+                    trackBarG.Value = selected.G;
+                    trackBarB.Value = selected.B;
+                    trackBarA.Value = selected.A;
+
+                    numR.Value = selected.R;
+                    numG.Value = selected.G;
+                    numB.Value = selected.B;
+                    numA.Value = selected.A;
+
+                    string hex = $"{selected.R:X2}{selected.G:X2}{selected.B:X2}{selected.A:X2}";
+                    txtHEXFormat.Text = hex;
+
+                    uint decimalValue = (uint)((selected.R << 24) | (selected.G << 16) | (selected.B << 8) | selected.A);
+                    txtDecimalFormat.Text = decimalValue.ToString();
+
+                    panelColor.BackColor = selected;
+
+                    isUpdating = false;
+                }
+            }
+        }
+    }
+}
